40be4128ded2dc8b53bf4c56c965057533cca4b2
pankajan <pankajan05@gmail.com>
2021-05-11 14:19:55 +0530

    Add project files.

---
+++ AspDotNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspDotNet.Startup))]
+namespace AspDotNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
